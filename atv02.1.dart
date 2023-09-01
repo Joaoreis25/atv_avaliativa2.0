8610bb7b2827080deba9c11e1069744bc12b1d41
João Guilherme <joaoguilhermeferreirareisgui@gmail.com>
2023-08-31 21:18:27 -0300

    segundo commit

---
+++ atv02.1.dart
@@ -0,0 +1,13 @@
+class Teste{
+  var estudante;
+  var nota;
+}
+  String getEstudante() => this.estudande;
+  void setEstudante(var estudante) => this.estudante
+  =estudante;
+
+int getNota() => this.nota;
+ void setNota(var nota) => this.nota = nota;
+}
+
+
